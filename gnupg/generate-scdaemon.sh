c320421fc88b45f63f24eb74f556dd82883f118a
Michael Mattie <codermattie@runbox.com>
2024-03-06 13:12:08 -0800

    (fix) fix the scdaemon config on darwin to not use external drivers for ccid

--- gnupg/generate-scdaemon.sh
+++ gnupg/generate-scdaemon.sh
@@ -1,14 +1,21 @@
 #! /usr/bin/env bash
 
-driver=/usr/lib/pcsc/drivers/serial/libccidtwin.so
-if [[ -f $driver ]]
+system=`uname`
+
+# On darwin use builtin ccid driver
+if [[ $system != "Darwin" ]]
 then
-  echo "pcsc-driver $driver"
-else
-  echo >/dev/stderr "WARNING! pcsc driver not found: $driver"
+  driver=/usr/lib/pcsc/drivers/serial/libccidtwin.so
+  if [[ -f $driver ]]
+  then
+    echo "pcsc-driver $driver"
+  else
+    echo >/dev/stderr "WARNING! pcsc driver not found: $driver"
+  fi
+
+  echo "disable-ccid"
 fi
 
-echo "disable-ccid"
 echo "debug-ccid-driver"
 echo "debug-level advanced"
 echo "log-file $HOME/.gnupg/scdaemon.log"
