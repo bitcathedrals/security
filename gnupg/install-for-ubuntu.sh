43f6a7524fad09ed6c7cb31c8eb82fa7835f0f7c
Michael Mattie <codermattie@runbox.com>
2024-03-06 15:32:18 -0800

    (fix) add pkcs11 software to ubuntu install

---
+++ gnupg/install-for-ubuntu.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+doas apt install gnupg pcsc-tools pcscd vsmartcard-vpcd scdaemon yubikey-manager libpam-yubico libpam-u2f libu2f-udev python3-ykman libykpers-1-1 pinentry-gnome3 pinentry-curses opensc-pkcs11 libengine-pkcs11-openssl gnupg-pkcs11-scd libpkcs11-helper1
+
+
+
+
+
--- gnupg/install-pcsc-for-ubuntu.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-doas apt install pcsc-tools pcscd vsmartcard-vpcd
